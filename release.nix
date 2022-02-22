eab6177eeabb897ab2f9e277fb7094a96068b1ec
Sander van der Burg <svanderburg@gmail.com>
2022-02-23 00:49:07 +0100

    Migrate to doxygen

--- release.nix
+++ release.nix
@@ -16,12 +16,12 @@ rec {
   dev = pkgs.lib.genAttrs systems (system: (import ./default.nix {
     inherit pkgs system;
   }).override (oldAttrs: {
-    buildInputs = [ pkgs.graphviz ];
+    buildInputs = [ pkgs.doxygen ];
     executable = true;
     postInstall = ''
-      vendor/bin/phpdoc
+      doxygen
       mkdir -p $out/nix-support
-      echo "doc api $out/share/php/composer-svanderburg-composer2nix/doc" >> $out/nix-support/hydra-build-products
+      echo "doc api $out/share/php/composer-svanderburg-composer2nix/doc/html" >> $out/nix-support/hydra-build-products
     '';
   }));
 
